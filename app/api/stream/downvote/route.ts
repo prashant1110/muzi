1aa6b4277d54debce5d2b62d942df9a595f09866
Prashant Singh <prashant@codetailor.in>
2024-10-23 16:30:55 +0530

    Complete backend

---
+++ app/api/stream/downvote/route.ts
@@ -0,0 +1,54 @@
+import { prismaClient } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const UpvoteSchema = z.object({
+  streamId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email || "",
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "UnAuthenticated",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  try {
+    const data = UpvoteSchema.parse(req.json());
+
+    await prismaClient.upVote.delete({
+      where: {
+        streamId_userId: {
+          userId: user?.id,
+          streamId: data.streamId,
+        },
+      },
+    });
+    return NextResponse.json({
+      message: "Done!",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error while downvoting",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const session = useSession();
-  console.log(session);
 
   return (
     <div>
