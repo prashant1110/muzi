0ad237e72e2f4cf10738963c576ad0c15402f78b
Prashant Singh <prashant@codetailor.in>
2024-10-25 11:10:56 +0530

    separate the components

---
+++ app/creator/[creatorId]/page.tsx
@@ -0,0 +1,9 @@
+import StreamView from "@/app/components/StreamView";
+
+export default function ({
+  params: { creatorId },
+}: {
+  params: { creatorId: string };
+}) {
+  return <StreamView creatorId={creatorId} />;
+}
